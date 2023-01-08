e72eed166cc7f7d5657f641e852524aa4dc19580
IsaacSunda <112872189+IsaacSunda@users.noreply.github.com>
2023-01-08 16:30:55 +0100

    Update settings.py

--- registration/settings.py
+++ registration/settings.py
@@ -54,7 +54,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "school.apps.SchoolConfig",
-    "phonenumber_field",
+#     "phonenumber_field",
     "accounts.apps.AccountsConfig",
 ]
 
